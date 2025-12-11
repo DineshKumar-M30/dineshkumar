5ad44c5b432263638f6fea605599fbf3644592a0
manoj <manojnataraj1712@gmail.com>
2025-12-11 22:35:07 +0530

    login page added

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js - PURE CSS VERSION (NO TAILWIND)
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
